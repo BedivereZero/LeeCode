744bd537ea4e2757f88ecadcc5aad217a3e3bf2f
BedivereZero@gmail.com <BedivereZero@gmail.com>
2017-04-01 17:22:37 +0800

    从下标 1 开始查重

--- algorithms/remove-duplicates-from-sorted-array.cpp
+++ algorithms/remove-duplicates-from-sorted-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 	public:
 		int removeDuplicates (vector<int>& nums) {
-			int i = 0;
+			int i = 1;
 			while(i < nums.size()) {
 				if (nums[i] == nums[i - 1])
 					nums.erase(nums.begin() + i);
