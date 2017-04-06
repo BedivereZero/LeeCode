40d3e9cec3d41ad51ebd306b0e3647e799a908ec
BedivereZero@gmail.com <BedivereZero@gmail.com>
2017-04-06 11:15:38 +0800

    空格结尾计数器不置为 0

--- algorithms/length-of-last-word.c
+++ algorithms/length-of-last-word.c
@@ -1,11 +1,17 @@
 int lengthOfLastWord(char* s) {
 	char* p = s;
 	int counter = 0;
+	bool new_word = false;
 	while (*p != '\0') {
 		if (*p == ' ')
-			counter = 0;
+			new_world = false;
 		else
-			counter++;
+			if (new_world) {
+				new_world = false;
+				counter = 1;
+			}
+			else
+				counter++;
 		p++;
 	}
 	return counter;
