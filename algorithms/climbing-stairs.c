746c93d07fd02cbb13f2c2849e08ff437362470f
BedivereZero@gmail.com <BedivereZero@gmail.com>
2017-04-11 11:41:38 +0800

    使用两个变量作为中间而非创建动态数组

--- algorithms/climbing-stairs.c
+++ algorithms/climbing-stairs.c
@@ -1,12 +1,13 @@
 int climbStairs(int n) {
-	int i = 0;
-	int s = 0;
-	int *p = (int *) malloc(n * sizeof(int));
-	p[0] = 1;
-	p[1] = 2;
-	for (i = 2; i < n; i++)
-		p[i] = p[i - 1] + p[i - 2];
-	s = p[n - 1];
-	free(p);
-	return s;
+	int a = 1;
+	int b = 1;
+	if (n < 2)
+		return 1;
+	else
+		n = n - 1;
+	while (n--) {
+		a = a + b;
+		b = a - b;
+	}
+	return a;
 }
