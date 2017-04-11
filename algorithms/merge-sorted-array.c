c6d6ba35570e1fa7c411a97a073f79a3e61ad845
BedivereZero@gmail.com <BedivereZero@gmail.com>
2017-04-11 14:25:55 +0800

    处理 nums2 全大于 nums1 的情况

--- algorithms/merge-sorted-array.c
+++ algorithms/merge-sorted-array.c
@@ -1,13 +1,16 @@
 void merge(int* nums1, int m, int* nums2, int n) {
-	if (!(m && n))
+	int i = 0;
+	int j = 0;
+	if (!n)
 		return;
-	for (i=m; i > 1; i--)
-		nums1[i] = nums1[i - 1];
-	if (nums1[0] < nums2[0])
-		nums1[1] = nums2[0];
-	else {
-		nums1[1] = nums1[0];
-		nums1[0] = nums2[0];
+	while (i < m) {
+		if (nums2[0] < nums1[i])
+			break;
+		else
+			i++;
 	}
-	return merge(nums1 + 2, m - 1, nums + 1, n - 1);
+	for (j=m; j > i; j--)
+		nums1[j] = nums1[j - 1];
+	nums1[i] = nums2[0];
+	return merge(nums1 + i + 1, m - i, nums2 + 1, n - 1);
 }
