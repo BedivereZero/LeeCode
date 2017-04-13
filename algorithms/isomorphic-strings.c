8288068f5ff2eab985751c73fc894a96d5fc0990
BedivereZero@gmail.com <BedivereZero@gmail.com>
2017-04-13 14:14:01 +0800

    使用库函数 strlen 求字符串长度

--- algorithms/isomorphic-strings.c
+++ algorithms/isomorphic-strings.c
@@ -1,14 +1,12 @@
 char* normalize(char* s) {
 	int i = 0;
-	int l = 0;
 	int m = 1;
 	int ch[256];
 	char* t = NULL;
-	for (l=0; s[l]; l++);
-	t = (char *) malloc(l * sizeof(char));
+	t = (char *) malloc(strlen(s) * sizeof(char));
 	for (i=0; i < 256; i++)
 		ch[i] = 0;
-	for (i=0; i < l; i++) {
+	for (i=0; i < strlen(s); i++) {
 		if (ch[s[i]] == 0) {
 			ch[s[i]] = m;
 			m++;
