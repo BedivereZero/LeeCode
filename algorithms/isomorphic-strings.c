0a0cf295ff93bc3b88aa0c0f5e4af4bf96c62eb6
BedivereZero@gmail.com <BedivereZero@gmail.com>
2017-04-13 13:38:03 +0800

    函数 normalize 处理范围由小写字母扩大为全部 ascii 码

--- algorithms/isomorphic-strings.c
+++ algorithms/isomorphic-strings.c
@@ -1,19 +1,19 @@
 char* normalize(char* s) {
 	int i = 0;
 	int l = 0;
-	int m = 0;
-	int ch[26];
+	int m = 1;
+	int ch[256];
 	char* t = NULL;
 	for (l=0; s[l]; l++);
 	t = (char *) malloc(l * sizeof(char));
-	for (i=0; i < 26; i++)
+	for (i=0; i < 256; i++)
 		ch[i] = 0;
 	for (i=0; i < l; i++) {
-		if (ch[s[i] - 'a'] == 0) {
-			ch[s[i] - 'a'] = m;
+		if (ch[s[i]] == 0) {
+			ch[s[i]] = m;
 			m++;
 		}
-		t[i] = 'a' + ch[s[i] - 'a'];
+		t[i] = ch[s[i]];
 	}
 	return t;
 }
