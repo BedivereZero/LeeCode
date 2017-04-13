46f0516d60cf8f2823564541f664c8e8af452b26
BedivereZero@gmail.com <BedivereZero@gmail.com>
2017-04-13 14:58:05 +0800

    相距最多为 k 即第二个下标的范围为 [i + 1, i + k + 1)

--- algorithms/contains-duplicate-ii.c
+++ algorithms/contains-duplicate-ii.c
@@ -2,7 +2,7 @@ bool containsNearbyDuplicate(int* nums, int numsSize, int k) {
 	int i = 0;
 	int j = 0;
 	for (i=0; i < numsSize - 1; i++)
-		for(j=i + 1; j < i + k; j++)
+		for(j=i + 1; j < i + k + 1; j++)
 			if (nums[i] == nums[j])
 				return true;
 	return false;
