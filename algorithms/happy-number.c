d28aab30f2c74b3595efcbec1c8e204a03e2e6d6
BedivereZero@gmail.com <BedivereZero@gmail.com>
2017-04-13 11:43:41 +0800

    边界条件修改, n < 10 时是否为 1 或 7

--- algorithms/happy-number.c
+++ algorithms/happy-number.c
@@ -1,7 +1,19 @@
 bool isHappy(int n) {
 	int s = 0;
+	int r[] = {
+		false,
+		true,
+		false,
+		false,
+		false,
+		false,
+		false,
+		true,
+		false,
+		false
+	};
 	if (n < 10)
-		return n == 1;
+		return r[n];
 	while(n) {
 		s += (n % 10) * (n % 10);
 		n = n / 10;
