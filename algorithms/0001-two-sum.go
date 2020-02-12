e34d52e4d75ff999da942e40d13834ec099155d4
BedivereZero <BedivereZero@gmail.com>
2020-02-12 21:51:07 +0800

    [0001] Golang Two sum
    Signed-off-by: BedivereZero <BedivereZero@gmail.com>

---
+++ algorithms/0001-two-sum.go
@@ -0,0 +1,11 @@
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		if j, e := m[target -nums[i]]; e {
+			return []int {j, i}
+		} else {
+			m[nums[i]] = i
+		}
+	}
+	return nil
+}
