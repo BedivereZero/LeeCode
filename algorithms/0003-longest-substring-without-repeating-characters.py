33eaeb077275dc711316b653837ab52296a9b2c8
BedivereZero <BedivereZero@gmail.com>
2020-02-18 23:04:26 +0800

    [0003] Python3 Use set to check if a string has no repeating characters
    Signed-off-by: BedivereZero <BedivereZero@gmail.com>

--- algorithms/0003-longest-substring-without-repeating-characters.py
+++ algorithms/0003-longest-substring-without-repeating-characters.py
@@ -3,6 +3,7 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
 
         l, x, y = 0, 0, 0
+        m = set()
 
         while True:
 
@@ -11,13 +12,23 @@ class Solution:
                 y += 1
                 if len(s) < y:
                     l = max(l, y - x - 1)
+                    # print('C: %3d, %3d, %3d, %s%s%s, %s' % (l, x, y, '_' * x, s[x:y], '_' * (len(s) - y), m))
                     return l
-                if len(set(s[x:y])) != len(s[x:y]):
+                if s[y - 1] in m:
                     break
+                else:
+                    m.add(s[y - 1])
             l = max(l, y - x - 1)
+            # print('A: %3d, %3d, %3d, %s%s%s, %s' % (l, x, y, '_' * x, s[x:y], '_' * (len(s) - y), m))
 
             # get longest substing without repeating characters endswith index y
             while True:
                 x += 1
                 if s[x - 1] == s[y - 1]:
                     break
+                m.remove(s[x - 1])
+            # print('B: %3d, %3d, %3d, %s%s%s, %s' % (l, x, y, '_' * x, s[x:y], '_' * (len(s) - y), m))
+
+
+if __name__ == '__main__':
+    Solution().lengthOfLongestSubstring('abcabcbb')
