4f18fb7b35ecb583be0c3189073c2d5b81cde397
BedivereZero <BedivereZero@gmail.com>
2020-05-18 22:21:42 +0800

    [0007] Python3 Enhance reverse integer
    Signed-off-by: BedivereZero <BedivereZero@gmail.com>

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+# Changelog
+
+| Difficulty | Resolved | Total |
+| :--------- | :------- | :---- |
+| Easy       | 2        | 492   |
+| Medium     | 2        | 837   |
+| Hard       | 0        | 328   |
+
+## [0007] - 2020-05-18
+
+### Python3
+
+- Transfer int to str.
+- Use `list[::-1]` to get reverse.
--- algorithms/0007-reverse-integer.py
+++ algorithms/0007-reverse-integer.py
@@ -1,17 +1,13 @@
 class Solution:
     def reverse(self, x: int) -> int:
+        if x < - (1 << 31) or not x < (1 << 31) - 1:
+            return 0
         negative = x < 0
+        strx = str(-x) if negative else str(x)
+        stry = strx[::-1]
         if negative:
-            x = -x
-        if x > 5927694924:
-            return 0
-        y = 0
-        while x:
-            y = y * 10 + x % 10
-            x = x // 10
-        y = -y if negative else y
-        if y > (1 << 31) - 1:
-            return 0
-        if y < - (1 << 31):
-            return 0
+            stry = '-' + stry
+        y = int(stry)
+        if y < - (1 << 31) or not y < (1 << 31) - 1:
+            y = 0
         return y
