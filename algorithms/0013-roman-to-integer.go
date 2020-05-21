20ccee39cf4985b1769b841c8229c7f466547e0f
BedivereZero <BedivereZero@gmail.com>
2020-05-21 22:32:18 +0800

    [0013] Golang Roman to integer
    Signed-off-by: BedivereZero <BedivereZero@gmail.com>

---
+++ algorithms/0013-roman-to-integer.go
@@ -0,0 +1,21 @@
+func romanToInt(s string) int {
+	cha := map[byte]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+	result := 0
+	for i := 0; i < len(s)-1; i++ {
+		if cha[s[i]] < cha[s[i+1]] {
+			result -= cha[s[i]]
+		} else {
+			result += cha[s[i]]
+		}
+	}
+	result += cha[s[len(s)-1]]
+	return result
+}
