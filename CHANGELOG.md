304c49d1f3db1e940a6c0171d86f3f757fd02fb3
BedivereZero <BedivereZero@gmail.com>
2020-05-21 22:14:38 +0800

    [0013] Python3 Compare next, add or minus
    Signed-off-by: BedivereZero <BedivereZero@gmail.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,7 +6,11 @@
 | Medium     | 2        | 837   |
 | Hard       | 0        | 328   |
 
-## [0009] Palindrome number - 2020-05-19
+## 0013 - Roman to integer
+
+- [Python3] Compare next, add or minus
+
+## 0009 - Palindrome number
 
 - [Python3] Numbers less than zero are not palindrome.
 - [Python3] Use `list[::-1]` get reversal.
@@ -14,7 +18,7 @@
 - [Golang] Numbers endswith zero are not palidrome.
 - [Golang] Reverse half.
 
-## [0007] Reverse interger - 2020-05-18
+## 0007 - Reverse interger
 
 - [Python3] Transfer int to str.
 - [Python3] Use `list[::-1]` to get reverse.
--- algorithms/0013-roman-to-integer.py
+++ algorithms/0013-roman-to-integer.py
@@ -1,6 +1,6 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        char = {
+        cha = {
             'I': 1,
             'V': 5,
             'X': 10,
@@ -8,24 +8,12 @@ class Solution:
             'C': 100,
             'D': 500,
             'M': 1000,
-            'IV': 4,
-            'IX': 9,
-            'XL': 40,
-            'XC': 90,
-            'CD': 400,
-            'CM': 900,
         }
-        length = len(s)
-        integer = 0
-        p = 0
-        while p < length:
-            if (length - p) < 2:
-                integer += char[s[p]]
-                break
-            if s[p:p+2] in char:
-                integer += char[s[p:p+2]]
-                p += 2
+        result = 0
+        for i in range(len(s) - 1):
+            if cha[s[i]] < cha[s[i + 1]]:
+                result -= cha[s[i]]
             else:
-                integer += char[s[p]]
-                p += 1
-        return integer
+                result += cha[s[i]]
+        result += cha[s[-1]]
+        return result
