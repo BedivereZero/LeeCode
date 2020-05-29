c38cf9a04c02d22ce5925e6e3795385ecb88fa37
BedivereZero <BedivereZero@gmail.com>
2020-05-29 21:53:26 +0800

    [Golang] Implement strstr
    Signed-off-by: BedivereZero <BedivereZero@gmail.com>

---
+++ algorithms/0028-implement-strstr.go
@@ -0,0 +1,9 @@
+func strStr(haystack string, needle string) int {
+	lh, ln := len(haystack), len(needle)
+	for i := 0; i < lh-ln+1; i++ {
+		if haystack[i:i+ln] == needle {
+			return i
+		}
+	}
+	return -1
+}
--- algorithms/0028-implement-strstr.py
+++ algorithms/0028-implement-strstr.py
@@ -1,11 +1,7 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        if haystack == "" and needle == "":
-            return 0
-        for i in range(len(haystack) - len(needle) + 1):
-            for j in range(len(needle)):
-                if haystack[i + j] != needle[j]:
-                    break
-            else:
+        m, n = len(haystack), len(needle)
+        for i in range(m - n + 1):
+            if haystack[i: i + n] == needle:
                 return i
         return -1
