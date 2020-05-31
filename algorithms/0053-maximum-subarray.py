9322a79b0b39bbeeaca4cc559e0dcbf31d20a261
BedivereZero <BedivereZero@gmail.com>
2020-05-31 19:57:52 +0800

    [0053] Python3 Divide and conquer
    Signed-off-by: BedivereZero <BedivereZero@gmail.com>

--- algorithms/0053-maximum-subarray.py
+++ algorithms/0053-maximum-subarray.py
@@ -5,8 +5,23 @@ class Solution:
         if len(nums) == 1:
             return nums[0]
         mid = len(nums) // 2
-        lmax = max(sum(nums[i:mid]) for i in range(mid))
-        rmax = max(sum(nums[mid:i]) for i in range(mid, len(nums) + 1))
+        if nums[:mid]:
+            lmax = nums[mid - 1]
+            cur = nums[mid - 1]
+            for i in range(mid - 2, -1, -1):
+                cur += nums[i]
+                if lmax < cur:
+                    lmax = cur
+        else:
+            lmax = 0
+        if nums[mid:]:
+            rmax, cur = nums[mid], nums[mid]
+            for i in range(mid + 1, len(nums)):
+                cur += nums[i]
+                if rmax < cur:
+                    rmax = cur
+        else:
+            rmax = 0
         cmax = lmax + rmax
         return max(self.maxSubArray(nums[:mid]), cmax, self.maxSubArray(nums[mid:]))
 
