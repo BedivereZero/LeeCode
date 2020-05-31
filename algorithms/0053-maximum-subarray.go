309cfd6b0cf9a1629512fde42bd0d0ccccbfd235
BedivereZero <BedivereZero@gmail.com>
2020-05-31 21:00:15 +0800

    [0053] Golang: DP
    Signed-off-by: BedivereZero <BedivereZero@gmail.com>

--- algorithms/0053-maximum-subarray.go
+++ algorithms/0053-maximum-subarray.go
@@ -1,15 +1,13 @@
 package algorithms
 
 func maxSubArray(nums []int) int {
+	max := nums[0]
 	for i := 1; i < len(nums); i++ {
 		if 0 < nums[i - 1] {
 			nums[i] = nums[i - 1] + nums[i]
 		}
-	}
-	max := nums[0]
-	for _, each := range nums[1:] {
-		if max < each {
-			max = each
+		if max < nums[i] {
+			max = nums[i]
 		}
 	}
 	return max
