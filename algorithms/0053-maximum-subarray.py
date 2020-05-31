498285179b6179246f183fb3ac4595574c3d857d
BedivereZero <BedivereZero@gmail.com>
2020-05-31 20:57:24 +0800

    [0053]: Python3: DP
    Signed-off-by: BedivereZero <BedivereZero@gmail.com>

--- algorithms/0053-maximum-subarray.py
+++ algorithms/0053-maximum-subarray.py
@@ -1,29 +1,12 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        if len(nums) == 0:
-            return 0
-        if len(nums) == 1:
-            return nums[0]
-        mid = len(nums) // 2
-        if nums[:mid]:
-            lmax = nums[mid - 1]
-            cur = nums[mid - 1]
-            for i in range(mid - 2, -1, -1):
-                cur += nums[i]
-                if lmax < cur:
-                    lmax = cur
-        else:
-            lmax = 0
-        if nums[mid:]:
-            rmax, cur = nums[mid], nums[mid]
-            for i in range(mid + 1, len(nums)):
-                cur += nums[i]
-                if rmax < cur:
-                    rmax = cur
-        else:
-            rmax = 0
-        cmax = lmax + rmax
-        return max(self.maxSubArray(nums[:mid]), cmax, self.maxSubArray(nums[mid:]))
-
-if __name__ == "__main__":
-    print(Solution().maxSubArray([1, 2]))
+        length = len(nums)
+        rec = nums[0]
+        i = 1
+        while i < length:
+            if nums[i - 1] > 0:
+                nums[i] = nums[i] + nums[i - 1]
+            if rec < nums[i]:
+                rec = nums[i]
+            i += 1
+        return rec
