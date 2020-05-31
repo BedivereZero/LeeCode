d386c5c9a322d44cbef7caa441c99f2b738fed5a
BedivereZero <BedivereZero@gmail.com>
2020-05-31 21:33:25 +0800

    [0066] Golang: Plus one
    Signed-off-by: BedivereZero <BedivereZero@gmail.com>

---
+++ algorithms/0066-plus-one.go
@@ -0,0 +1,19 @@
+func reverse(input []int) []int {
+	if len(input) == 0 {
+		return input
+	} else {
+		return(append(reverse(input[1:]), input[0]))
+	}
+}
+
+func plusOne(digits []int) []int {
+	digits = reverse(digits)
+	c := 1
+	for i := 0; i < len(digits); i++ {
+		digits[i], c = (digits[i] + c) % 10, (digits[i] + c) / 10
+	}
+	if c > 0 {
+		digits = append(digits, c)
+	}
+	return reverse(digits)
+}
