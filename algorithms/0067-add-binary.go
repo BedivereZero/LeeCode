91dbbf57da6f5a8e7d5e90491f3eaa3a826d6c4e
BedivereZero <BedivereZero@gmail.com>
2020-06-01 19:36:30 +0800

    [0067] Golang: No reverse
    Signed-off-by: BedivereZero <BedivereZero@gmail.com>

--- algorithms/0067-add-binary.go
+++ algorithms/0067-add-binary.go
@@ -1,41 +1,30 @@
-func reverse(input string) string {
-	if len(input) > 0 {
-		return reverse(input[1:]) + string([]byte{input[0]})
-	} else {
-		return input
-	}
-}
+package algorithms
 
 func addBinary(a string, b string) string {
-	a = reverse(a)
-	b = reverse(b)
+	la, lb := len(a), len(b)
 	r := ""
 	c := 0
-	for i :=0; i < len(a) || i < len(b); i++ {
-		var x, y int
-		if i < len(a) {
-			x = int(a[i] - '0')
-		} else {
-			x = 0
+	for i := 0; i < la || i < lb; i++ {
+		x, y := 0, 0
+		if la - i - 1 >= 0 {
+			x = int(a[la - i - 1] - '0')
 		}
-		if i < len(b) {
-			y = int(b[i] - '0')
-		} else {
-			y = 0
+		if lb - i - 1 >= 0 {
+			y = int(b[lb - i - 1] - '0')
 		}
 		switch x + y + c {
 		case 0:
-			c, r = 0, r + "0"
+			c, r = 0, "0" + r
 		case 1:
-			c, r = 0, r + "1"
+			c, r = 0, "1" + r
 		case 2:
-			c, r = 1, r + "0"
+			c, r = 1, "0" + r
 		case 3:
-			c, r = 1, r + "1"
+			c, r = 1, "1" + r
 		}
 	}
 	if c > 0 {
-		r += "1"
+		r = "1" + r
 	}
-	return reverse(r)
+	return r
 }
