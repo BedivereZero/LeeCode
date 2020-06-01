c0466be18beccb9bd4face686cad6e353772a609
BedivereZero <BedivereZero@gmail.com>
2020-06-01 19:49:49 +0800

    [0067] Python3: No reverse
    Signed-off-by: BedivereZero <BedivereZero@gmail.com>

--- algorithms/0067-add-binary.py
+++ algorithms/0067-add-binary.py
@@ -1,32 +1,12 @@
 class Solution:
-    def reverse_str(self, input: str) -> str:
-        if input:
-            return self.reverse_str(input[1:]) + input[0]
-        else:
-            return input
-    
     def addBinary(self, a: str, b: str) -> str:
-        a = self.reverse_str(a)
-        b = self.reverse_str(b)
-        t = {
-            ('0', '0', '0'): ('0', '0'),
-            ('0', '0', '1'): ('0', '1'),
-            ('0', '1', '0'): ('0', '1'),
-            ('0', '1', '1'): ('1', '0'),
-            ('1', '0', '0'): ('0', '1'),
-            ('1', '0', '1'): ('1', '0'),
-            ('1', '1', '0'): ('1', '0'),
-            ('1', '1', '1'): ('1', '1'),
-        }
         r = str()
-        i = 0
-        c = '0'
-        while i < len(a) or i < len(b):
-            x = a[i] if i < len(a) else '0'
-            y = b[i] if i < len(b) else '0'
-            c, s = t[(x, y, c)]
-            r = r + s
-            i += 1
-        if c == '1':
-            r = r + c
-        return self.reverse_str(r)
+        c = 0
+        for i in range(max(len(a), len(b))):
+            x = ord(a[len(a) - i - 1]) - ord('0') if i <= len(a) - 1 else 0
+            y = ord(b[len(b) - i - 1]) - ord('0') if i <= len(b) - 1 else 0
+            c, s = (x + y +c) // 2, (x + y + c) % 2
+            r = chr(s + ord('0')) + r
+        if c:
+            r = '1' + r
+        return r
