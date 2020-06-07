5a22cc143749ac7b153ffc7bf17366f2c6970445
BedivereZero <BedivereZero@gmail.com>
2020-06-07 14:51:09 +0800

    [0017] Golang: Letter combinations of a phone number
    Signed-off-by: BedivereZero <BedivereZero@gmail.com>

---
+++ algorithms/0017-letter-combinations-of-a-phone-number.go
@@ -0,0 +1,36 @@
+func letterCombinations(digits string) []string {
+	var out []string
+	for _, i := range digits {
+		var cur, cs []string
+		switch i {
+		case '1':
+			return cur
+		case '2':
+			cs = []string {"a", "b", "c"}
+		case '3':
+			cs = []string {"d", "e", "f"}
+		case '4':
+			cs = []string {"g", "h", "i"}
+		case '5':
+			cs = []string {"j", "k", "l"}
+		case '6':
+			cs = []string {"m", "n", "o"}
+		case '7':
+			cs = []string {"p", "q", "r", "s"}
+		case '8':
+			cs = []string {"t", "u", "v"}
+		case '9':
+			cs = []string {"w", "x", "y", "z"}
+		}
+		for _, c := range cs {
+			if len(out) == 0 {
+				cur = append(cur, c)
+			}
+			for _, e := range out {
+				cur = append(cur, e + c)
+			}
+		}
+		out = cur
+	}
+	return out
+}
