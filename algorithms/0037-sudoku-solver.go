d1c114330e6fff9585f737bad3f8d8d4d30e35b4
BedivereZero <BedivereZero@gmail.com>
2020-07-03 21:51:28 +0800

    [0037] Golang: Sudoku Solver
    Signed-off-by: BedivereZero <BedivereZero@gmail.com>

--- algorithms/0037-sudoku-solver.go
+++ algorithms/0037-sudoku-solver.go
@@ -1,90 +1,64 @@
-// package algorithms
-package main
+package algorithms
 
-import (
-	"errors"
-	"fmt"
-)
-
-func main() {
-	b := [][]byte{{'.', '8', '7', '6', '5', '4', '3', '2', '1'}, {'2', '.', '.', '.', '.', '.', '.', '.', '.'}, {'3', '.', '.', '.', '.', '.', '.', '.', '.'}, {'4', '.', '.', '.', '.', '.', '.', '.', '.'}, {'5', '.', '.', '.', '.', '.', '.', '.', '.'}, {'6', '.', '.', '.', '.', '.', '.', '.', '.'}, {'7', '.', '.', '.', '.', '.', '.', '.', '.'}, {'8', '.', '.', '.', '.', '.', '.', '.', '.'}, {'9', '.', '.', '.', '.', '.', '.', '.', '.'}}
-	fmt.Println(isValidSudoku(b))
+func solveSudoku(board [][]byte) {
+	solve(board, 0)
 }
 
-func isValidSudoku(board [][]byte) bool {
-	return valid(board, 0, 0)
-}
-
-func next(board [][]byte, x int, y int) (int, int, error) {
-	for i := x; i < 9; i++ {
-		for j := y; j < 9; j++ {
-			if board[i][j] == '.' {
-				return i, j, nil
-			}
-		}
-	}
-	return 0, 0, errors.New("not found")
-}
-
-func valid(board [][]byte, x int, y int) bool {
-	for i := range board {
-		for j := range board[i] {
-			fmt.Printf(" %c", board[i][j])
-		}
-		fmt.Printf("\n")
-	}
-	fmt.Printf("valid: %d, %d\n", x, y)
-
-	x, y, err := next(board, x, y)
-	fmt.Printf("next: %d, %d\n", x, y)
-
-	if err != nil {
+func solve(board [][]byte, i int) bool {
+	var x, y int
+	i = next(board, i)
+	if i < 81 {
+		x, y = i/9, i%9
+	} else {
 		return true
 	}
-
-	candidate := getCandidate(board, x, y)
-
+	candidate := getCandiate(board, x, y)
 	for _, c := range candidate {
 		board[x][y] = c
-		if valid(board, (x*9+y+1)/9, (x*9+y+1)%9) {
+		if ok := solve(board, i+1); ok {
 			return true
+		} else {
+			board[x][y] = '.'
 		}
 	}
-	board[x][y] = '.'
-
 	return false
 }
 
-func getCandidate(board [][]byte, x int, y int) []byte {
-	// fmt.Printf("A: x: %d, y: %d\n", x, y)
-	flag := make([]bool, 9)
-	// same column
-	for i := 0; i < 9; i++ {
-		if board[i][y] != '.' {
-			flag[int(board[i][y]-'0'-1)] = true
+func next(board [][]byte, i int) int {
+	for i < 81 {
+		if board[i/9][i%9] == '.' {
+			break
 		}
+		i++
 	}
-	// same row
+	return i
+}
+
+func getCandiate(board [][]byte, x int, y int) []byte {
+	flag := make([]bool, 9)
+	// check row
 	for i := 0; i < 9; i++ {
 		if board[x][i] != '.' {
-			flag[int(board[x][i]-'0'-1)] = true
+			flag[int(board[x][i]-'1')] = true
 		}
 	}
-	// same block
-	for i := x / 3 * 3; i < x/3*3+3; i++ {
-		for j := y / 3 * 3; j < y/3*3+3; j++ {
-			if board[i][j] != '.' {
-				// fmt.Printf("B: x: %d, y: %d, f: %d\n", i, j, int(board[i][j]-'0'))
-				flag[int(board[i][j]-'0'-1)] = true
-			}
+	// check column
+	for i := 0; i < 9; i++ {
+		if board[i][y] != '.' {
+			flag[int(board[i][y]-'1')] = true
 		}
 	}
-
-	r := []byte{}
+	// check block
+	for i := 0; i < 9; i++ {
+		if board[x/3*3+i/3][y/3*3+i%3] != '.' {
+			flag[int(board[x/3*3+i/3][y/3*3+i%3]-'1')] = true
+		}
+	}
+	var c []byte
 	for i, f := range flag {
 		if !f {
-			r = append(r, byte('0'+i+1))
+			c = append(c, byte('1'+i))
 		}
 	}
-	return r
+	return c
 }
