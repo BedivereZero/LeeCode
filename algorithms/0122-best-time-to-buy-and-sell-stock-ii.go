47752fe872dea00e74fa9240dabc4fb562f98348
BedivereZero <BedivereZero@gmail.com>
2020-07-16 13:43:07 +0800

    [0122] Golang: Optimize judgment
    Signed-off-by: BedivereZero <BedivereZero@gmail.com>

--- algorithms/0122-best-time-to-buy-and-sell-stock-ii.go
+++ algorithms/0122-best-time-to-buy-and-sell-stock-ii.go
@@ -3,13 +3,9 @@ package algorithms
 func maxProfit(prices []int) int {
 	sum := 0
 	for i := 1; i < len(prices); i++ {
-		if prices[i] < prices[i-1] {
-			sum += prices[i-1] - prices[0]
-			prices[0] = prices[i]
+		if prices[i] > prices[i-1] {
+			sum += prices[i] - prices[i-1]
 		}
-	}
-	if prices[0] < prices[len(prices)-1] {
-		sum += prices[len(prices)-1] - prices[0]
 	}
 	return sum
 }
