a9e326f5588a45235150fe24edefb7bca743175e
BedivereZero <BedivereZero@gmail.com>
2020-07-17 13:18:41 +0800

    [0134] Golang: Traverse once
    Signed-off-by: BedivereZero <BedivereZero@gmail.com>

--- algorithms/0134-gas-station.go
+++ algorithms/0134-gas-station.go
@@ -1,10 +1,29 @@
 package algorithms
 
+import "log"
+
+func canCompleteCircuit(gas []int, cost []int) int {
+	index := 0
+	for i := 0; i < len(gas); i++ {
+		gas[i] = gas[i] - cost[i]
+		if i > 0 {
+			gas[i] += gas[i-1]
+			if gas[i] < gas[index] {
+				index = i
+			}
+		}
+	}
+	if gas[len(gas)-1] < 0 {
+		return -1
+	}
+	return (index + 1) % len(gas)
+}
 func canCompleteCircuit(gas []int, cost []int) int {
 	// gen remain
 	for i := 0; i < len(gas); i++ {
 		gas[i] = gas[i] - cost[i]
 	}
+	log.Println("A:", gas)
 
 	index := 0
 	for i := 1; i < len(gas); i++ {
@@ -13,6 +32,27 @@ func canCompleteCircuit(gas []int, cost []int) int {
 			index = i
 		}
 	}
+	log.Println("B:", gas)
+	if gas[len(gas)-1] < 0 {
+		return -1
+	}
+	return (index + 1) % len(gas)
+}
+func canCompleteCircuit(gas []int, cost []int) int {
+	// gen remain
+	for i := 0; i < len(gas); i++ {
+		gas[i] = gas[i] - cost[i]
+	}
+	log.Println("A:", gas)
+
+	index := 0
+	for i := 1; i < len(gas); i++ {
+		gas[i] += gas[i-1]
+		if gas[i] < gas[index] {
+			index = i
+		}
+	}
+	log.Println("B:", gas)
 	if gas[len(gas)-1] < 0 {
 		return -1
 	}
